caf7443f6d3eaf127b2b01cacec7fcfdb4f33092
Skylar Lynner <skylar.lynner@gmail.com>
2022-11-10 12:07:06 -0600

    home

--- react-ui/src/App.js
+++ react-ui/src/App.js
@@ -1,14 +1,16 @@
 import { Routes, Route } from "react-router-dom";
 import Footer from "./components/Footer";
 import Header from "./components/Header";
+import Home from "./components/Home";
 import NotFound from "./components/NotFound";
 
 function App() {
   return (
     <div className="app">
       <Header />
-      
+
       <Routes>
+        <Route path="/" element={<Home />} />
         <Route path="*" element={<NotFound />} />
       </Routes>
 
---
+++ react-ui/src/components/Home/index.js
@@ -0,0 +1,22 @@
+export default function Home() {
+    return (
+        <main className="container mt-3">
+            <h2 className="d-flex justify-content-center">Bullet Journal Bookshelf</h2>
+            
+            <section>
+                <p>
+                    Keep all of your reading notes together in a single place.
+                    Add books to your bookshelf, track your reading habits, 
+                    and save your favorite quotes. 
+                </p>
+                <p>
+                    Inspired by the use of a Bullet Journal to keep track of 
+                    reading, a journey with learning a new language, and the 
+                    note taking system created by Ryan Holiday that 
+                    encourages organizing notes from reading to actively 
+                    engage in the process.
+                </p>
+            </section>
+        </main>
+    );
+}
