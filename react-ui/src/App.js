745a37d1791aa5ba3d0a666930d8cdcee260b8e7
Skylar Lynner <skylar.lynner@gmail.com>
2022-11-10 11:35:54 -0600

    header

--- react-ui/src/App.js
+++ react-ui/src/App.js
@@ -1,10 +1,13 @@
 import { Routes, Route } from "react-router-dom";
 import Footer from "./components/Footer";
+import Header from "./components/Header";
 import NotFound from "./components/NotFound";
 
 function App() {
   return (
     <div className="app">
+      <Header />
+      
       <Routes>
         <Route path="*" element={<NotFound />} />
       </Routes>
---
+++ react-ui/src/components/Header/NavBar.js
@@ -0,0 +1,17 @@
+import { NavLink } from "react-router-dom";
+
+export default function NavBar() {
+    return (
+        <nav className="navbar navbar-expand-md navbar-dark bg-dark">
+            <div className="container-fluid">
+                <div className="navbar-header">
+                    <NavLink to="/" className="navbar-brand"><img height="32" src="./book-icon.png" alt="logo" />Bookshelf</NavLink>
+                </div>
+                <ul className="nav navbar-nav">
+                    <li className="nav-item"><NavLink to="/login" className="nav-link">Login</NavLink></li>
+                    <li className="nav-item"><NavLink to="/register" className="nav-link">Register</NavLink></li>
+                </ul>
+            </div>
+        </nav>
+    );
+}
