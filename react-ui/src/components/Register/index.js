2537b7bf3eebec961be2c2ed51530207109df4e6
Skylar Lynner <skylar.lynner@gmail.com>
2022-11-10 15:33:48 -0600

    useReducer message using dispatch

--- react-ui/src/components/Register/index.js
+++ react-ui/src/components/Register/index.js
@@ -1,14 +1,47 @@
 import { useReducer } from "react";
 
+const initialState = {
+    message: "hi"
+};
+
+function reducer(state, action) {
+    switch(action.type) {
+        case "yell":
+            return {
+                message: `HEY! I JUST SAID ${state.message}`
+            };
+        case "whisper":
+            return {
+                message: "excuse me..."
+            };
+        default:
+            return {
+                message: "hello"
+            };
+    }
+}
+
 export default function Register() {
-    const [ number, setNumber ] =useReducer(
-        (number, newNumber) => number + newNumber,
-        0
+    const [ state, dispatch ] = useReducer(
+        reducer,
+        initialState
     );
 
     return (
         <main className="container mt-3">
-            <h2 onClick={() => setNumber(1)}>{number}</h2>
+            <p>Message: {state.message}</p>
+            <button 
+                className="btn btn-secondary me-2" 
+                onClick={() => dispatch({ type: "yell" })}
+            >
+                YELL
+            </button>
+            <button 
+                className="btn btn-secondary" 
+                onClick={() => dispatch({ type: "whisper" })}
+            >
+                whisper
+            </button>
         </main>
     );
 }
