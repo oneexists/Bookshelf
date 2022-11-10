c4fd456bf957e6e8d425bf8f73a087958624fb83
Skylar Lynner <skylar.lynner@gmail.com>
2022-11-10 14:03:52 -0600

    useState star rating

--- react-ui/src/components/Register/index.js
+++ react-ui/src/components/Register/index.js
@@ -3,10 +3,44 @@ import { useState } from "react";
 export default function Register() {
     const [ checked, setChecked ] = useState(false);
 
+    const createArray = (length) => [
+        ...Array(length)
+    ];
+
+    function RatingDisplay({ selected = false, onSelect }) {
+        return (
+            <button 
+                className={selected ? "btn btn-danger" : "btn btn-secondary"}
+                onClick={onSelect}
+            ></button>
+        );
+    }
+
+    function Rating({ total = 5 }) {
+        const[ selected, setSelected ] = useState(0);
+
+        return (
+            <p>
+                <span className="me-3">
+                    {createArray(total).map((n, i) => (
+                        <RatingDisplay 
+                        key={i} 
+                        selected={selected > i} 
+                        onSelect={() => setSelected(i + 1)} 
+                        />
+                    ))}
+                </span>
+                {selected} of {total}
+            </p>
+        );
+    }
+
     return (
         <main className="container mt-3">
             <h2 className="d-flex justify-content-center">Bookshelf Registration</h2>
             
+            <p>Rating: {<Rating total={10}/>}</p>
+            <p>Default Rating: {<Rating />}</p>
             <form>
                 <div className="form-check mb-3">
                     <input 
