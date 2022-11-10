b5e2fdeb80c2b96bcf26da839d70bd0c96823443
Skylar Lynner <skylar.lynner@gmail.com>
2022-11-10 14:30:05 -0600

    useEffect

--- react-ui/src/components/Register/index.js
+++ react-ui/src/components/Register/index.js
@@ -1,58 +1,32 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 export default function Register() {
-    const [ checked, setChecked ] = useState(false);
+    const [ name, setName ] = useState("");
+    const [ submittedName, setSubmittedName ] = useState("");
 
-    const createArray = (length) => [
-        ...Array(length)
-    ];
-
-    function RatingDisplay({ selected = false, onSelect }) {
-        return (
-            <button 
-                className={selected ? "btn btn-danger" : "btn btn-secondary"}
-                onClick={onSelect}
-            ></button>
-        );
-    }
-
-    function Rating({ total = 5 }) {
-        const[ selected, setSelected ] = useState(0);
-
-        return (
-            <p>
-                <span className="me-3">
-                    {createArray(total).map((n, i) => (
-                        <RatingDisplay 
-                        key={i} 
-                        selected={selected > i} 
-                        onSelect={() => setSelected(i + 1)} 
-                        />
-                    ))}
-                </span>
-                {selected} of {total}
-            </p>
-        );
-    }
+    useEffect(() => {
+        document.title = `Hello ${submittedName}`;
+    });
 
     return (
         <main className="container mt-3">
             <h2 className="d-flex justify-content-center">Bookshelf Registration</h2>
-            
-            <p>Rating: {<Rating total={10}/>}</p>
-            <p>Default Rating: {<Rating />}</p>
-            <form>
-                <div className="form-check mb-3">
-                    <input 
-                        type="checkbox" 
-                        className="form-check-input" 
-                        name="checked"
-                        value={checked} 
-                        onChange={() => setChecked((checked) => !checked)} 
-                    />
-                    <label htmlFor="checked" className="form-check-label">{checked ? "checked" : "not checked"}</label>
-                </div>
-            </form>
+
+            <div className="form-group mb-3">
+                <label htmlFor="name" className="form-label">Name:</label>
+                <input 
+                    type="text"
+                    aria-label="name"
+                    aria-required="true"
+                    className="form-control"
+                    id="name"
+                    value={name}
+                    onChange={(evt) => setName(evt.target.value)}
+                    required
+                />
+            </div>
+
+            <button type="submit" className="btn btn-success" onClick={() => setSubmittedName(name)}>Submit</button>
         </main>
     );
 }
