162d0707f952a326a09e37b0451f711c3f7065b2
Skylar Lynner <skylar.lynner@gmail.com>
2022-11-11 09:52:10 -0600

    custom fetch hook

--- react-ui/src/App.js
+++ react-ui/src/App.js
@@ -1,26 +1,13 @@
 import { Routes, Route } from "react-router-dom";
-import { createContext, useContext } from "react";
 import Footer from "./components/Footer";
 import Header from "./components/Header";
 import Home from "./components/Home";
 import NotFound from "./components/NotFound";
 import Register from "./components/Register";
 
-export const TreesContext = createContext();
-
-export const useTrees = () => 
-  useContext(TreesContext);
-  
-const trees = [
-    { id: 1, type: "Maple" },
-    { id: 2, type: "Oak" },
-    { id: 3, type: "Family" },
-    { id: 4, type: "Maple" }
-];
 
 function App() {  
   return (
-    <TreesContext.Provider value={{trees}}>
       <div className="app">
         <Header />
 
@@ -32,7 +19,6 @@ function App() {
 
         <Footer />
       </div>
-    </TreesContext.Provider>
   );
 }
 
--- react-ui/src/components/Register/index.js
+++ react-ui/src/components/Register/index.js
@@ -1,18 +1,30 @@
-import { useTrees } from "../../App";
+import { useFetch } from "../../hooks/useFetch";
 
 export default function Register() {
-    const { trees } = useTrees();
+    const { loading, data, error } = useFetch(`http://localhost:8080/api/appUsers`);
 
+    if (loading) {
+        return (
+            <main className="container mt-3">
+                <h1>loading</h1>
+            </main>
+        );
+    }
+    if (error) {
+        return (
+            <main className="container mt-3">
+                <pre>{JSON.stringify(error, null, 2)}</pre>
+            </main>
+        );
+    }
     return (
         <main className="container mt-3">
-            <div>
-                <h1>Types of Trees</h1>
-                <ul>
-                    {trees.map((t) => (
-                        <li key={t.id}>{t.type}</li>
-                    ))}
-                </ul>
-            </div>
+            <p>Total Users: {data._embedded.appUsers.length}</p>
+            <p><a href={data._links.self.href}>Self</a></p>
+            <p><a href={data._links.profile.href}>Profile</a></p>
+            <p><a href={data._links.search.href}>Search Link</a></p>
+            <p>Total Pages: {data.page.totalPages}</p>
+            <pre>{JSON.stringify(data, null, 2)}</pre>
         </main>
     );
 }
