337f4d1d90f623270b0f743e456fe93735dc7f84
Skylar Lynner <skylar.lynner@gmail.com>
2022-11-15 13:05:28 -0600

    modal components

--- react-ui/src/components/Bookshelf/index.js
+++ react-ui/src/components/Bookshelf/index.js
@@ -1,24 +1,13 @@
-import { findAll } from "../../services/bookService";
+import { findById } from "../../services/bookService";
 import { LargeBookListItem } from "./Book/LargeBookListItem";
-import { SmallBookListItem } from "./Book/SmallBookListItem";
-import { NumberedList } from "./NumberedList";
-import { RegularList } from "./RegularList";
+import { Modal } from "./Modal";
 
 export default function Bookshelf() {
     return (
         <main className="container mt-3">
-            <h1>Small Book List</h1>
-            <NumberedList 
-                items={findAll()} 
-                resourceName="book" 
-                itemComponent={SmallBookListItem}
-            />
-            <h1>Large Book List</h1>
-            <RegularList 
-                items={findAll()} 
-                resourceName="book" 
-                itemComponent={LargeBookListItem}
-            />
+            <Modal>
+                <LargeBookListItem book={findById(1)} />
+            </Modal>
         </main>
     );
 }
