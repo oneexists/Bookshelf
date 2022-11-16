7c6937fc42efa2904bdf8123d7495b868a673150
Skylar Lynner <skylar.lynner@gmail.com>
2022-11-16 10:08:32 -0600

    data source component

--- react-ui/src/components/Bookshelf/index.js
+++ react-ui/src/components/Bookshelf/index.js
@@ -1,10 +1,15 @@
 import { BookLoader } from "./Book/BookLoader";
 import { BookInfo } from "./Book/BookInfo";
 import { ResourceLoader } from "./ResourceLoader";
+import { DataSource } from "./DataSource";
+import { findById } from "../../services/bookService";
 
 export default function Bookshelf() {
     return (
         <main className="container mt-3">
+            <DataSource getDataFunc={findById(2)} resourceName="book">
+                <BookInfo />
+            </DataSource>
             <ResourceLoader resourceUrl="http://localhost:8080/api/books/1" resourceName="book">
                 <BookInfo />
             </ResourceLoader>
