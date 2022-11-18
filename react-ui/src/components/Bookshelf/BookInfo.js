f1c083066484f495e927f47486eab0df77cb03ce
Skylar Lynner <skylar.lynner@gmail.com>
2022-11-18 10:18:28 -0600

    useDataSource hook

--- react-ui/src/components/Bookshelf/BookInfo.js
+++ react-ui/src/components/Bookshelf/BookInfo.js
@@ -1,8 +1,13 @@
-import { useResource } from "../../hooks/useResource";
+import { useDataSource } from "../../hooks/useDataSource";
 import { AuthorInfo } from "./AuthorInfo";
 
+const serverResource = resourceUrl => async() => {
+    const response = await fetch(resourceUrl);
+    return response.json();
+}
+
 export const BookInfo = ({ bookId }) => {
-    const book = useResource(`http://localhost:8080/api/books/${bookId}`);
+    const book = useDataSource(serverResource(`http://localhost:8080/api/books/${bookId}`));
     const { title, pages, language, _links } = book || {};
     const { author } = _links || {};
     const { href } = author || {};
