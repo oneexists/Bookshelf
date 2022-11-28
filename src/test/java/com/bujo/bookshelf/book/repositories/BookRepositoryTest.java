199c2fa06a5aa8f45610129002506c7eb46e9ee0
Skylar Lynner <skylar.lynner@gmail.com>
2022-11-28 10:37:13 -0600

    find and delete by id testing

--- src/test/java/com/bujo/bookshelf/book/repositories/BookRepositoryTest.java
+++ src/test/java/com/bujo/bookshelf/book/repositories/BookRepositoryTest.java
@@ -20,11 +20,8 @@ class BookRepositoryTest {
     @Autowired
     JdbcTemplate jdbcTemplate;
 
-    /**
-     * @throws java.lang.Exception
-     */
     @BeforeEach
-    void setUp() throws Exception {
+    void setUp() {
         jdbcTemplate.update("call set_known_good_state();");
     }
 
@@ -34,5 +31,39 @@ class BookRepositoryTest {
 
         assertNotNull(actual);
         assertEquals(2, actual.size());
+    }
+
+    @Test
+    void testShouldFindById() {
+        Book expected = getExpectedBook();
+        Book actual = repository.findById(1L).orElse(null);
+
+        assertNotNull(actual);
+        assertEquals(expected.getTitle(), actual.getTitle());
+        assertEquals(expected.getLanguage(), actual.getLanguage());
+        assertEquals(expected.getPages(), actual.getPages());
+    }
+
+    Book getExpectedBook() {
+        Book expected = new Book();
+        expected.setTitle("War and Peace");
+        expected.setLanguage("English");
+        expected.setPages(1296);
+
+        return expected;
+    }
+
+    @Test
+    void testShouldNotFindByMissingId() {
+        Book actual = repository.findById(1_000_000L).orElse(null);
+
+        assertNull(actual);
+    }
+
+    @Test
+    void testShouldDeleteById() {
+        repository.deleteById(2L);
+
+        assertNull(repository.findById(2L).orElse(null));
     }
 }
