ced5c19632ee1dab9b276a8732d5de6d47b917f0
Skylar Lynner <skylar.lynner@gmail.com>
2022-12-15 15:47:29 -0600

    replace table with card components

---
+++ react-ui/src/components/Bookshelf/BookCards/BookCard.js
@@ -0,0 +1,22 @@
+import BookDetails from "../BookView/BookDetails";
+import SecondaryNavLinkButton from "../../buttons/SecondaryNavLinkButton";
+import WarningNavLinkButton from "../../buttons/WarningNavLinkButton";
+
+export default function BookCard({ book }) {
+    const { bookId, title, author, language, pages } = book;
+
+    return (
+        <div className="col mb-3" key={bookId}>
+            <div className="card">
+                <div className="card-body">
+                    <h5 className="card-title">{title}</h5>
+                    <BookDetails author={author} language={language} pages={pages} />
+                </div>
+                <div className="card-footer d-flex justify-content-center">
+                    <SecondaryNavLinkButton url={`/books/${bookId}`} text="View Book" />
+                    <WarningNavLinkButton url={`/books/${bookId}/edit`} text="Edit Book" />
+                </div>
+            </div>
+        </div>
+    );
+}
---
+++ react-ui/src/components/Bookshelf/BookCards/index.js
@@ -0,0 +1,14 @@
+import { ComponentList } from "../../layouts/ComponentList";
+import BookCard from "./BookCard";
+
+export default function BookCards({ books }) {
+    return (
+        <div className="row row-cols-3 g2">
+            <ComponentList 
+                items={books}
+                resourceName="book"
+                itemComponent={BookCard}
+            />
+        </div>
+    )
+}
