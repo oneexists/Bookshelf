39b7d75645cc555a6a771a1c866cf4e06ea9bd63
Skylar Lynner <skylar.lynner@gmail.com>
2022-12-22 16:26:57 -0600

    refactor button bars and repetitive css

--- react-ui/src/components/buttons/DangerButton.js
+++ react-ui/src/components/buttons/DangerButton.js
@@ -1,3 +1,3 @@
-export default function DangerButton({ text }) {
-    return <button type="button" className="btn btn-danger w-100">{text}</button>;
+export default function DangerButton({ text, marginEnd, handleClick }) {
+    return <button type="button" className={`btn btn-danger w-100 me-${marginEnd}`} onClick={handleClick}>{text}</button>;
 }
--- react-ui/src/components/buttons/WarningNavLinkButton.js
+++ react-ui/src/components/buttons/WarningNavLinkButton.js
@@ -1,5 +1,5 @@
 import { NavLink } from "react-router-dom";
 
-export default function WarningNavLinkButton({ url, text }) {
-    return <NavLink to={url} className="btn btn-warning w-100" role="button">{text}</NavLink>;
+export default function WarningNavLinkButton({ url, text, marginEnd }) {
+    return <NavLink to={url} className={`btn btn-warning w-100 me-${marginEnd}`} role="button">{text}</NavLink>;
 }
