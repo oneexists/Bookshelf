8bfce2641ee71908884abda182ff88e4f2178e5d
Skylar Lynner <skylar.lynner@gmail.com>
2023-01-03 15:42:19 -0600

    dynamically display current year

--- react-ui/src/components/Footer/index.js
+++ react-ui/src/components/Footer/index.js
@@ -1,7 +1,7 @@
 export default function Footer() {
     return (
         <footer className="container mt-3">
-            &copy;2022 | Skylar Lynner
+            &copy;{new Date().getFullYear()} | Skylar Lynner
         </footer>
     );
 }
