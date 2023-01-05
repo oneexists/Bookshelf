900d36e0b23986de1b22d46c172871a4a454bd92
Skylar Lynner <skylar.lynner@gmail.com>
2023-01-05 13:32:51 -0600

    remove arrow function from delete button

--- react-ui/src/features/Bookshelf/components/BookView/BookViewButtonBar.js
+++ react-ui/src/features/Bookshelf/components/BookView/BookViewButtonBar.js
@@ -4,13 +4,18 @@ import ButtonBar from "../../../../components/layouts/ButtonBar";
 import DangerButton from "../../../../components/buttons/DangerButton";
 import { useNavigate } from "react-router-dom";
 import { deleteBookById } from "../../../../services/bookService";
+import { memo, useCallback } from "react";
+
+const DeleteButton = memo(({ onClick }) => (
+    <DangerButton text="Delete Book" handleClick={onClick} />
+));
 
 export default function BookViewButtonBar({bookId}) {
     const navigate = useNavigate();
 
-    const handleDelete = () => {
+    const handleDelete = useCallback(() => {
         deleteBookById(bookId).then(navigate("/"));
-    }
+    }, [bookId, navigate]);
 
     return (
         <ButtonBar>
@@ -18,7 +23,7 @@ export default function BookViewButtonBar({bookId}) {
             <SecondaryNavLinkButton url="quotes/add" text="Add a Quote" marginEnd={2} />
             <SecondaryNavLinkButton url="notes/add" text="Add a Note" marginEnd={2} />
             <WarningNavLinkButton url="edit" text="Edit Book Details" marginEnd={2} />
-            <DangerButton text="Delete Book" handleClick={handleDelete} />
+            <DeleteButton onClick={handleDelete} />
         </ButtonBar>
     );
 }
