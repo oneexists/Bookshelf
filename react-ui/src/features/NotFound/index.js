7db73b39e02a293c3408406abdfd8b3ec3366acd
Skylar Lynner <skylar.lynner@gmail.com>
2023-01-16 12:33:01 -0600

    use local image for not found page

--- react-ui/src/features/NotFound/index.js
+++ react-ui/src/features/NotFound/index.js
@@ -6,7 +6,7 @@ export default function NotFound() {
             <div className="row d-flex justify-content-center">
                 <section className="mr-auto ml-auto text-center">
                     <img  
-                        src="https://t3.ftcdn.net/jpg/01/67/37/06/240_F_167370626_zEV8195DIsvDnCJHIkBbBHgIotS4vPKl.jpg" 
+                        src="img/not-found-coffee.jpg" 
                         alt="spilled coffee" 
                     />
                     <p>The page you were looking for could not be found.</p>
