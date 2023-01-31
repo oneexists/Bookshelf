135e6be55a531a2ac436951745b2c71e8f0eb90c
Skylar Lynner <skylar.lynner@gmail.com>
2023-01-31 12:38:16 -0600

    extract auth into withBooks hoc

--- react-ui/src/features/Bookshelf/hoc/withBooks.js
+++ react-ui/src/features/Bookshelf/hoc/withBooks.js
@@ -1,15 +1,17 @@
 import React, { useEffect, useState } from "react";
+import { useAuth } from "../../../hooks/useAuth";
 import { findUserBooks } from "../../../services/bookService";
 
-export const withBooks = (Component, id, message) => {
+export const withBooks = (Component, message) => {
     return props => {
+        const auth = useAuth();
         const [ books, setBooks ] = useState([]);
     
         useEffect(() => {
-            findUserBooks({ id }).then(b => {
+            findUserBooks({ id: auth.user.id }).then(b => {
                 setBooks(b._embedded.books);
             });
-        }, []);
+        }, [auth.user.id]);
     
         return books.length > 0 
             ? <Component { ...props } books={books} /> 
