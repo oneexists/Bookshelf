53bf5dae334aaf0619cebbde0a76f47317a4dc14
Skylar Lynner <skylar.lynner@gmail.com>
2023-01-31 12:38:16 -0600

    extract auth into withBooks hoc

--- react-ui/src/features/Bookshelf/components/bookshelves/AllBooks/index.js
+++ react-ui/src/features/Bookshelf/components/bookshelves/AllBooks/index.js
@@ -1,12 +1,10 @@
-import { useAuth } from "../../../../../hooks/useAuth";
 import { withBooks } from "../../../hoc/withBooks";
 import BookCards from "../BookCards";
 import BookshelfSplitLayout from "../../../layouts/BookshelfSplitLayout";
 
 export default function AllBooks() {
     const MESSAGE = "Add a book to get started!";
-    const auth = useAuth();
-    const BookCardsWithLoader = withBooks(BookCards, auth.user.id, MESSAGE);
+    const BookCardsWithLoader = withBooks(BookCards, MESSAGE);
 
     return (
         <BookshelfSplitLayout 
