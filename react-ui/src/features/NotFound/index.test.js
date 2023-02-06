6039673d1540bbd0d265d23f11fab7eb31d0be2f
Skylar Lynner <skylar.lynner@gmail.com>
2023-02-06 12:54:50 -0600

    assert expected title text

--- react-ui/src/features/NotFound/index.test.js
+++ react-ui/src/features/NotFound/index.test.js
@@ -1,13 +1,16 @@
 import { render, screen } from "@testing-library/react";
 import "@testing-library/jest-dom";
-import NotFound from ".";
+import NotFound from "./index";
 
-test("not found displays image and paragraph", () => {
+test("not found displays title, image and paragraph", () => {
     render(<NotFound />);
     
+    const heading = screen.getByRole("heading");
     const image = screen.getByRole("img");
     const paragraph = screen.getByText(/The page you were looking for/);
     
+    expect(heading).toBeInTheDocument();
+    expect(heading).toHaveTextContent("That was strange...");
     expect(image).toBeInTheDocument();
     expect(paragraph).toBeInTheDocument();
 });
