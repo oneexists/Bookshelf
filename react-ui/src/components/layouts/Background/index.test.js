bd8274d566c2dd95e184d3c654a692d4342d0d8e
Skylar Lynner <skylar.lynner@gmail.com>
2023-02-06 12:54:15 -0600

    assert expected classes

--- react-ui/src/components/layouts/Background/index.test.js
+++ react-ui/src/components/layouts/Background/index.test.js
@@ -1,10 +1,11 @@
 import { render, screen } from "@testing-library/react";
 import Background from "./index";
 
-test("background renders main", () => {
+test("background renders main with classes", () => {
     render(<Background />);
 
     const main = screen.getByRole("main");
     
     expect(main).toBeInTheDocument();
+    expect(main).toHaveClass("container p-3");
 });
